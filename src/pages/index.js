bab21afbc769aa18e601f84de391030f0bb147e1
BraeuerMax <maximilian@c-braeuer.de>
2023-05-24 21:31:56 +0200

    Updated index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import * as React from "react"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <div>
+        <h1>Hello World!</h1>
+        <h2>Designing a Gatsby site</h2>
+        <p>This is my Homepage</p>
+      </div>
+    </Layout>
+  )
 }
